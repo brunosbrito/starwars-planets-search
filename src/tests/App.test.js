15cfd9a4eb795fb3fac78abb11eb21128bb3f3b0
Bruno <brunobrito728@gmail.com>
2022-11-23 15:30:36 -0300

    req-05 finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
+import StarWarsProvider from '../context/StarWarsProvider'
 
-test('I am your test', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Hello, App!/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Teste do projeto Star Wars Planets', () => {
+  
+  it('verifica se tem um input pesquisar na tela', () => {
+    render(<StarWarsProvider >
+            <App />
+          </StarWarsProvider> )
+
+    const nameFilter = 'name-filter';
+    const inputName = screen.getByTestId(nameFilter)
+    expect(inputName).toBeInTheDocument();
+  })
+})
